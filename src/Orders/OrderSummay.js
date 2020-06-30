000f243b4e9031fb6fe7e2505a65c2db7dde4e36
Samish <bedisamish@gmail.com>
2020-07-01 01:45:21 +0530

    Fixing wrong item and delivery total in order summary page

--- src/Orders/OrderSummay.js
+++ src/Orders/OrderSummay.js
@@ -164,7 +164,7 @@ const OrderSummary = (props) => {
                     <Grid item xs={3} style={{textAlign: 'end'}}>
                         <Typography variant="body2" noWrap={true} 
                             className={classes.itemsTotal}>
-                            &#8377;{Number(order.totalPrice) - Number(order.deliveryFee) <= 0 ? 0 : Number(order.deliveryFee)}
+                            &#8377;{Number(order.totalPrice) - Number(order.deliveryFee) <= 0 ? 0 : Number(order.totalPrice) - Number(order.deliveryFee)}
                         </Typography>
                     </Grid>
                 </Grid>
@@ -180,7 +180,7 @@ const OrderSummary = (props) => {
                     <Grid item xs={3} style={{textAlign: 'end'}}>
                         <Typography variant="body2" noWrap={true} 
                             className={classes.itemsTotal}>
-                            {Number(order.totalPrice) <= 0 ? 'FREE' : <React.Fragment>&#8377;{order.deliveryFee}</React.Fragment>}
+                            {(Number(order.totalPrice) <= 0 || Number(order.deliveryFee) <= 0) ? 'FREE' : <React.Fragment>&#8377;{order.deliveryFee}</React.Fragment>}
                         </Typography>
                     </Grid>
                 </Grid>
